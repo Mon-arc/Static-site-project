0db02362696aa6cbc2ad285820538186625721a3
Mon-arc <hassanelshikh25@gmail.com>
2025-01-27 23:51:06 -0500

    can't remember changes lmao

--- src/main.py
+++ src/main.py
@@ -1,8 +1,31 @@
+import os
+from os.path import isfile
 from textnode import *
+import shutil
 
 def main():
-    node = TextNode("Meow", TextType.BOLD, "https://www.boot.dev")
-    print(node)
+    copy_static_to_public("static/", "public")
+
+
+
+def copy_static_to_public(src_dir, dest_dir, first_call=True):
+    if first_call and os.path.exists(dest_dir):
+        shutil.rmtree(dest_dir)
+    if first_call:
+        os.mkdir(dest_dir)
+    for item in os.listdir(src_dir):
+        src_path = os.path.join(src_dir, item)
+        dest_path = os.path.join(dest_dir, item)
+
+        if os.path.isfile(src_path):
+            shutil.copy(src_path, dest_path)
+            print(src_path)
+        else:
+            os.mkdir(dest_path)
+            copy_static_to_public(src_path, dest_path, False)
+
+
+
 
 if __name__ == "__main__":
     main()
--- test_file.py
+++
@@ -1,2 +0,0 @@
-text = "###Meow"
-print('###' in text)
